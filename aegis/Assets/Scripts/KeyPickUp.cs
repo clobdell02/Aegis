0dfbe1ec4de5efa9b1c57d04a8c4c3ea902c89e1
Colton Lobdell <83106043+clobdell02@users.noreply.github.com>
2022-05-10 14:01:04 -0700

    key pickupable
    added the script for the player character that allows for the key dropped by keyholders to be picked up.

---
+++ aegis/Assets/Scripts/KeyPickUp.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickUp : MonoBehaviour
+{
+    public float keyCount = 0.0f; // used to track the amount of keys a player has picked up
+
+    // Start is called before the first frame update
+    void Start()
+    { }
+
+    // Update is called once per frame
+    void Update()
+    { }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Key"))
+        {
+            other.gameObject.SetActive(false);
+            keyCount += 1;
+        }
+    }
+}
