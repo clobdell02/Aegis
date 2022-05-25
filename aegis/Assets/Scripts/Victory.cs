3a260eaa778c0f8bf74a276d77cc498dc62900ca
Ethan Pressley <ethanpressley321@gmail.com>
2022-05-24 23:12:56 -0700

    updated victory code

--- aegis/Assets/Scripts/Victory.cs
+++ aegis/Assets/Scripts/Victory.cs
@@ -7,6 +7,24 @@ public class Victory : MonoBehaviour
 {
   void OnTriggerEnter(Collider other)
   {
-    SceneManager.LoadScene("Victory");
+    
+    Scene scene = SceneManager.GetActiveScene();
+
+    switch(scene.name)
+    {
+      case "level1 Desert":
+        SceneManager.LoadScene("level2 Forest");
+        break;
+      case "level2 Forest":
+        SceneManager.LoadScene("level3 Cliffs");
+        break;
+      case "level3 Cliffs":
+        SceneManager.LoadScene("Victory");
+        break;
+      default:
+        SceneManager.LoadScene("Title");
+        break;
+    }
+    
   }
 }
