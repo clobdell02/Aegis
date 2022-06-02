1ad0a60dcf4255ae5e8b52fb7a46879ab7ab440b
cpendergraft98 <cpendy98@gmail.com>
2022-06-01 20:34:32 -0700

    Level Music Done
    Background music and ambiance implemented for all levels and menus, as well as a defeat sound effect. Still need audio for the following: enemy takes damage, enemy dies, player victory, projectiles fired, player footsteps (having trouble finding audio that syncs with the animation), projectile blocked (when hitting shield)

--- aegis/Assets/Scripts/Player/PlayerControllerv2.cs
+++ aegis/Assets/Scripts/Player/PlayerControllerv2.cs
@@ -21,7 +21,6 @@ public class PlayerControllerv2 : MonoBehaviour
   public static int lives = 3;
   private Animator animator;
   static float keyCount;
-  public AudioSource FootstepAudio;
 
     public void SetLivesText()
     {
@@ -52,17 +51,6 @@ public class PlayerControllerv2 : MonoBehaviour
       bool isWalking = (hasHorizontalInput || hasVerticalInput) && !ThirdPersonDash.inDash;
 
       animator.SetBool("IsWalking", isWalking);
-      if(isWalking)
-      {
-        if(!FootstepAudio.isPlaying)
-        {
-          FootstepAudio.Play(); // Need to find decent footstep sfx
-        }
-      }
-      else
-      {
-        FootstepAudio.Stop();
-      }
 
       Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
