6d4408ae9768dd60f971dbf38700b34075fb38aa
cpendergraft98 <cpendy98@gmail.com>
2022-06-02 18:30:14 -0700

    Added basic instructions to level 1

--- aegis/Assets/Scripts/KeyCreator.cs
+++ aegis/Assets/Scripts/KeyCreator.cs
@@ -6,10 +6,11 @@ public class KeyCreator : MonoBehaviour
 {
     // This script is designed to be attached to something in the scene that acts as
     // an observer. I simply create an empty object and attach the script to it
-    
+
     public int enemiesNeeded; // Number of gold enemies the player needs to defeat to spawn object (set in inspector)
     static public int enemiesDefeated; // Number of enemies the player has defeated
     private GameObject victoryItem; // Actual victoryItem to be spawned
+    private bool alreadySpawned; // Want to make sure we only spawn one object
 
     void Start()
     {
@@ -19,14 +20,17 @@ public class KeyCreator : MonoBehaviour
 
       // Instantiate the number of enemies defeated to 0
       enemiesDefeated = 0;
+
+      alreadySpawned = false;
     }
 
     void Update()
     {
-      if((enemiesDefeated >= enemiesNeeded))
+      if((enemiesDefeated >= enemiesNeeded) && (alreadySpawned == false))
       {
         Debug.Log("Victory Item Spawned");
         victoryItem.gameObject.SetActive(true);
+        alreadySpawned = true;
       }
     }
 }
